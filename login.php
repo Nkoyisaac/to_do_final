435fb4cd7fcc5a1bc64214b779592ba65d234f6c
Isaac Nkoy <nkoylokoteisaacdelor@gmail.com>
2019-10-04 10:18:54 +0200

    creation of a login form

--- login.php
+++ login.php
@@ -9,27 +9,21 @@
 <body>
 <div class="container">
     <div class="header">
-        <h2>Register</h2>
+        <h2>Log in</h2>
     </div>
     <form action="">
         <div>
             <label for="username">Username</label>
             <input type="text" name="username">
         </div>
-        <div>
-            <label for="email">Email</label>
-            <input type="text" name="email">
-        </div>
+      
         <div>
             <label for="password">Password</label>
             <input type="password" name="password_1">
         </div>
-        <div>
-            <label for="password">Confirm Password</label>
-            <input type="password" name="password_2">
-        </div>
-        <button type="submit">Register</button>
-        <p>Already a user <a href="login.php"><b>Log in</b></a></p>
+        
+        <button type="submit">Log in</button>
+        <p>don't have an account <a href="registration.php"><b>Register</b></a></p>
     </form>
 </div>
     
