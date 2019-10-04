5745029b48c1916f9d040b428e5e3cdab42ab5a0
Isaac Nkoy <nkoylokoteisaacdelor@gmail.com>
2019-10-04 10:10:40 +0200

    creation of the registration page

---
+++ registration.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>registration</title>
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <h2>Register</h2>
+    </div>
+    <form action="">
+        <div>
+            <label for="username">Username</label>
+            <input type="text" name="username">
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="text" name="email">
+        </div>
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password_1">
+        </div>
+        <div>
+            <label for="password">Confirm Password</label>
+            <input type="password" name="password_2">
+        </div>
+        <button type="submit">Register</button>
+    </form>
+</div>
+    
+</body>
+</html>
